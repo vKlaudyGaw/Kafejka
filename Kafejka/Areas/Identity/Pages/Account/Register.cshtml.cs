778b7f7328eb12088f907aad24993d21a1a24e93
vKlaudyGaw <klaudia030303@gmail.com>
2025-01-19 03:57:55 +0100

    css zmiany i identity strony (?)

--- Kafejka/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Kafejka/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -84,7 +84,7 @@ namespace Kafejka.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "Has³o musi miec od 6 do 100 znaków.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -95,7 +95,7 @@ namespace Kafejka.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Has³a s¹ ró¿ne od siebie.")]
             public string ConfirmPassword { get; set; }
         }
 
